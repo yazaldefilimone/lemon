5d00d20c47f172a7e2c074d982b599233fbcc5ce
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-08-07 00:16:18 +0200

    *

--- src/token_reader.rs
+++ src/token_reader.rs
@@ -1,4 +1,8 @@
-use crate::token::Token;
+use crate::error;
+use crate::messages::Messages;
+use crate::token::{Token, TokenKind};
+
+type Result<T> = std::result::Result<T, ()>;
 
 pub struct TokenReader<'a> {
 	position: usize,
@@ -6,4 +10,134 @@ pub struct TokenReader<'a> {
 	source: &'a str,
 }
 
-impl<'a> TokenReader<'a> {}
+impl<'a> TokenReader<'a> {
+	pub fn new(tokens: Vec<Token<'a>>, source: &'a str) -> Self {
+		Self { position: 0, tokens, source }
+	}
+
+	pub fn next(&mut self, messages: &mut Messages) -> Result<&Token<'a>> {
+		if self.position >= self.tokens.len() {
+			let message = error!("unexpected end of file");
+			messages.message(message);
+			return Err(());
+		}
+		let token = &self.tokens[self.position];
+		self.position += 1;
+		Ok(token)
+	}
+	pub fn rewind(&mut self, n: usize) {
+		self.position = self.position.saturating_sub(n);
+	}
+
+	pub fn next_ahead(&mut self, messages: &mut Messages) -> Result<&Token<'a>> {
+		if let Ok(ahead_token) = self.next_n(1) {
+			return Ok(ahead_token);
+		}
+		let message = error!("unexpected end of file");
+		messages.message(message);
+		return Err(());
+	}
+
+	pub fn peek(&self) -> Result<&Token<'a>> {
+		if self.position >= self.tokens.len() {
+			return Err(());
+		}
+		Ok(&self.tokens[self.position])
+	}
+
+	pub fn peek_kind(&self) -> Result<&TokenKind> {
+		self.peek().map(|token| &token.kind)
+	}
+
+	pub fn peek_ahead(&self) -> Result<&Token<'a>> {
+		let ahead_position = self.position + 1;
+		if ahead_position >= self.tokens.len() {
+			return Err(());
+		}
+		Ok(&self.tokens[ahead_position])
+	}
+	pub fn peek_two_ahead(&self) -> Result<&Token<'a>> {
+		let two_position_ahead = self.position + 2;
+		if two_position_ahead >= self.tokens.len() {
+			return Err(());
+		}
+		Ok(&self.tokens[two_position_ahead])
+	}
+
+	pub fn expect(&mut self, kind: TokenKind, messages: &mut Messages) -> Result<Token<'a>> {
+		let token = self.peek()?.clone();
+		if token.kind != kind {
+			let message = error!("expected '{}', found '{}'", kind, token.kind);
+			messages.message(message);
+			return Err(());
+		}
+		self.next(messages)?;
+		Ok(token)
+	}
+
+	pub fn expect_word(&mut self, word: &str, messages: &mut Messages) -> Result<Token<'a>> {
+		let token = self.expect(TokenKind::Word, messages)?;
+		if token.text != word {
+			let message = error!("expected '{word}', found '{token}'");
+			messages.message(message);
+			return Err(());
+		}
+		Ok(token)
+	}
+
+	pub fn read_if_word(&mut self, word: &str, messages: &mut Messages) -> Result<Token<'a>> {
+		match self.peek().cloned() {
+			Ok(Token { kind: TokenKind::Word, text, .. }) if text == word => {
+				self.next(messages)?;
+				Ok(Token { kind: TokenKind::Word, text, span: self.peek()?.span })
+			}
+			_ => Err(()),
+		}
+	}
+
+	pub fn read_semicolon(&mut self, messages: &mut Messages) -> Result<Token<'a>> {
+		let token = self.peek()?.clone();
+		match token {
+			Token { kind: TokenKind::Semicolon, .. } => {
+				self.next(messages)?;
+				Ok(token)
+			}
+			_ => Err(()),
+		}
+	}
+
+	#[inline]
+	pub fn read_newlines(&mut self) {
+		while self.peek().map(|token| token.kind) == Ok(TokenKind::Newline) {
+			self.position += 1;
+		}
+	}
+
+	pub fn source(&self) -> &'a str {
+		self.source
+	}
+
+	fn peek_n(&self, n: usize) -> Result<&Token<'a>> {
+		let ahead_position = self.position + n;
+		if ahead_position >= self.tokens.len() {
+			return Err(());
+		}
+		Ok(&self.tokens[ahead_position])
+	}
+
+	fn peek_n_kind(&self, n: usize) -> Result<&TokenKind> {
+		self.peek_n(n).map(|token| &token.kind)
+	}
+
+	pub fn end(&self) -> bool {
+		self.position >= self.tokens.len()
+	}
+	fn next_n(&mut self, n: usize) -> Result<&Token<'a>> {
+		let ahead_position = self.position + n;
+		if ahead_position >= self.tokens.len() {
+			return Err(());
+		}
+		self.position += n;
+		Ok(&self.tokens[ahead_position])
+	}
+}
