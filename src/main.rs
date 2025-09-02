3206ca711ca204352d5943d8bef9bfb7069a135a
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-09-02 17:45:52 +0200

    fix: suport new version of cli

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ mod macros;
 mod messages;
 mod mir;
 mod parser;
+mod reference;
 mod resolver;
 mod span;
+mod symbols;
 mod token;
 mod token_reader;
 
@@ -25,12 +27,12 @@ fn emit(stages: &[cli::EmitStage], path: &std::path::Path) {
 	let mut files = Vec::new();
 	let index = files.len() as u32;
 	if let Err(e) = load_single_file(path.to_path_buf(), &mut files) {
-		eprintln!("error loading file: {}", e);
+		eprintln!("error: {}", e);
 		std::process::exit(1);
 	}
 	let file = files.get(index as usize).unwrap();
 	match stages {
-		[cli::EmitStage::Ast] => {
+		[cli::EmitStage::AST] => {
 			let mut messages = Messages::new(file);
 			let mut lexer = lexer::Lexer::new(index, &file.source);
 			let mut token_reader = lexer.reader(&mut messages);
@@ -38,7 +40,7 @@ fn emit(stages: &[cli::EmitStage], path: &std::path::Path) {
 			messages.print("parser");
 			println!("{:#?}", file);
 		}
-		[cli::EmitStage::Mir] => {
+		[cli::EmitStage::HIR] => {
 			let mut messages = Messages::new(file);
 			let mut lexer = lexer::Lexer::new(index, &file.source);
 			let mut token_reader = lexer.reader(&mut messages);
@@ -46,7 +48,7 @@ fn emit(stages: &[cli::EmitStage], path: &std::path::Path) {
 			messages.print("parser");
 			println!("{:?}", file);
 		}
-		[cli::EmitStage::LlvmIr] => {
+		[cli::EmitStage::LLVMIR] => {
 			let mut messages = Messages::new(file);
 			let mut lexer = lexer::Lexer::new(index, &file.source);
 			let mut token_reader = lexer.reader(&mut messages);
@@ -54,7 +56,7 @@ fn emit(stages: &[cli::EmitStage], path: &std::path::Path) {
 			messages.print("parser");
 			println!("{:?}", file);
 		}
-		[cli::EmitStage::Hir] => {
+		[cli::EmitStage::MIR] => {
 			let mut messages = Messages::new(file);
 			let mut lexer = lexer::Lexer::new(index, &file.source);
 			let mut token_reader = lexer.reader(&mut messages);
@@ -70,7 +72,7 @@ fn print(item: cli::PrintItem, path: &std::path::Path) {
 	let mut files = Vec::new();
 	let index = files.len() as u32;
 	if let Err(e) = load_single_file(path.to_path_buf(), &mut files) {
-		eprintln!("error loading file: {}", e);
+		eprintln!("error: {}", e);
 		std::process::exit(1);
 	}
 	let file = files.get(index as usize).unwrap();
@@ -87,7 +89,7 @@ fn print(item: cli::PrintItem, path: &std::path::Path) {
 			println!("errors: {}", messages.list.len());
 			messages.print("lexer");
 		}
-		cli::PrintItem::Span => println!("span"),
+		cli::PrintItem::Spans => println!("span"),
 		cli::PrintItem::Types => println!("types"),
 	}
 }
@@ -98,7 +100,7 @@ fn build() {
 
 fn main() {
 	let arguments = cli::parse_args();
-	let file = arguments.input.to_path_buf();
+	let file = arguments.input_file.to_path_buf();
 	// let output = arguments.output.as_ref().map(|p| p.to_path_buf());
 
 	// let mut resolver = resolver::Resolver::new(file);
@@ -107,9 +109,9 @@ fn main() {
 	// let ast = resolver.ast();
 
 	match arguments.mode() {
-		cli::Mode::Emit(stages) => emit(&stages, &file),
-		cli::Mode::Print(item) => print(item, &file),
+		cli::Mode::EmitStages(stages) => emit(&stages, &file),
+		cli::Mode::Inspect(item) => print(item, &file),
 		cli::Mode::Build => build(),
-		cli::Mode::Check => check(),
+		cli::Mode::CheckOnly => check(),
 	}
 }
