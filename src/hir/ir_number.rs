96bcbd00c0f997737a5f3486bc180004acd8e702
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-10-01 15:31:52 +0200

    *

--- src/hir/ir_number.rs
+++ src/hir/ir_number.rs
@@ -4,7 +4,7 @@ use rust_decimal::prelude::ToPrimitive;
 use rust_decimal::Decimal;
 
 use crate::{
-	checker::{context::store::TypeStore, types::TypeId},
+	checker::types::{store::TypeStore, TypeId},
 	error,
 	messages::Messages,
 	span::Span,
