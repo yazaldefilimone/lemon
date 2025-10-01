6e5d72c5d8ad24ae9903e3b57eadf6acf6665dd5
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-10-01 15:31:52 +0200

    *

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,9 +1,22 @@
+use crate::{ast, messages::Message};
+
 pub mod context;
+pub mod expressions;
+pub mod functions;
+pub mod statements;
 pub mod synthesis;
 pub mod types;
-
-pub mod expressions;
-pub mod statements;
 pub mod variables;
 
-pub fn check(ctx: &mut context::Context, module: &ast::Module) {}
+pub type Result<T> = std::result::Result<T, Message>;
+
+pub fn check_file<'a, 'b>(
+	ctx: &mut context::Context<'a, 'b>,
+	file: &'a ast::File<'a>,
+) -> Result<()> {
+	match synthesis::synthesise_block(ctx, &file.block) {
+		Ok(typed) => {}
+		Err(message) => return Err(message),
+	}
+	return Ok(());
+}
