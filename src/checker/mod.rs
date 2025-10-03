4815c1981e3bfde1fda196588bcacfff5d5fb1e8
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-10-03 12:04:32 +0200

    *

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,5 +1,4 @@
-use crate::{ast, messages::Message};
-
+use crate::{ast, hir, messages::Message};
 pub mod context;
 pub mod expressions;
 pub mod functions;
@@ -13,10 +12,23 @@ pub type Result<T> = std::result::Result<T, Message>;
 pub fn check_file<'a, 'b>(
 	ctx: &mut context::Context<'a, 'b>,
 	file: &'a ast::File<'a>,
-) -> Result<()> {
+) -> Result<hir::File<'a>> {
+	let block = check_block(ctx, &file.block)?;
+
 	match synthesis::synthesise_block(ctx, &file.block) {
 		Ok(typed) => {}
 		Err(message) => return Err(message),
 	}
-	return Ok(());
+	return Ok(hir::File::new(block, &file.file));
+}
+
+pub fn check_block<'a, 'b>(
+	ctx: &mut context::Context<'a, 'b>,
+	block: &'a ast::Block<'a>,
+) -> Result<hir::Block<'a>> {
+	let mut hir_block = hir::Block::new();
+	for statement in &block.statements {
+		// let statement = check_statement(ctx, statement)?;
+	}
+	return Ok(hir_block);
 }
